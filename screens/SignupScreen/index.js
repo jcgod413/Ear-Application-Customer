e260d4c61c7866a4faef4997bfa5d5b68e4a5345
“jcgod413” <jcgod413@gmail.com>
2018-02-01 03:39:32 +0900

    Signup Screen view

--- screens/SignupScreen/index.js
+++ screens/SignupScreen/index.js
@@ -1,22 +1,214 @@
 import React from "react";
 import {
+  StyleSheet,
   Text,
+  TextInput,
   View,
-  StyleSheet
+  Keyboard,
+  Dimensions,
+  StatusBar,
+  Picker,
+  ScrollView,
+  TouchableHighlight
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import SimplePicker from "react-native-simple-picker";
+
+const { width, height } = Dimensions.get("window");
 
 const SignupScreen = props => (
   <View style={styles.container}>
-    <Text>Signup Screen!</Text>
+    <StatusBar barStyle={"dark-content"} />
+    <View style={styles.header}>
+      <Text style={styles.title}>회원가입</Text>
+      <TouchableHighlight
+        style={styles.close}
+        onPress={() => props.navigation.goBack()}
+      >
+        <Ionicons name={"md-close"} color={"#4a4a4a"} size={24} />
+      </TouchableHighlight>
+    </View>
+    <ScrollView>
+      <View style={styles.body1}>
+        <TextInput
+          placeholder="이름을 입력하세요"
+          style={styles.textInput}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+        />
+        <View style={styles.pickerContainer}>
+          <View style={styles.picker}>
+            <Text style={styles.pickerText}>거주시</Text>
+            <Ionicons
+              name={"ios-arrow-down"}
+              style={styles.pickerIcon}
+              color={"#bbb"}
+              size={24}
+            />
+          </View>
+          <View style={styles.picker}>
+            <Text style={styles.pickerText}>거주구</Text>
+            <Ionicons
+              name={"ios-arrow-down"}
+              style={styles.pickerIcon}
+              color={"#bbb"}
+              size={24}
+            />
+          </View>
+        </View>
+        <TextInput
+          placeholder="이메일을 입력하세요 (선택)"
+          style={styles.textInput}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+        />
+      </View>
+      <View style={styles.body2}>
+        <TextInput
+          placeholder="휴대전화번호"
+          style={styles.textInput}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="인증번호"
+          style={styles.textInput}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="비밀번호"
+          style={styles.textInput}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+        />
+        <TextInput
+          placeholder="비밀번호 재확인"
+          style={styles.textInput}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+        />
+      </View>
+      <View style={styles.body3}>
+        <Text style={styles.agreeText}>개인정보 수집이용 동의 (필수)</Text>
+        <Text style={styles.moreText}>내용보기</Text>
+      </View>
+      <TouchableHighlight
+        style={styles.signinButton}
+        onPress={() => {
+          this._onPressButton("signin");
+        }}
+      >
+        <Text style={styles.signinText}>가입하기</Text>
+      </TouchableHighlight>
+    </ScrollView>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 24,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "flex-start"
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20
+  },
+  title: {
+    height: 30,
+    marginTop: 25,
+    marginLeft: 20,
+    fontWeight: "800",
+    fontSize: 28,
+    letterSpacing: 0,
+    textAlign: "left",
+    color: "#4a4a4a"
+  },
+  close: {
+    marginTop: 14,
+    marginRight: 24
+  },
+  body1: {
+    width,
+    alignItems: "center",
+    marginTop: 24
+  },
+  body2: {
+    width,
+    alignItems: "center",
+    marginTop: 11
+  },
+  body3: {
+    width,
+    alignItems: "flex-start",
+    marginTop: 17,
+    marginLeft: 33
+  },
+  textInput: {
+    height: 60,
+    borderColor: "#bbb",
+    borderWidth: StyleSheet.hairlineWidth,
+    width: width - 40,
+    paddingHorizontal: 15,
+    backgroundColor: "#FAFAFA",
+    fontSize: 24
+  },
+  pickerContainer: {
+    flexDirection: "row",
+    marginHorizontal: 20
+  },
+  picker: {
+    flex: 1,
+    height: 60,
+    borderColor: "#bbb",
+    borderWidth: StyleSheet.hairlineWidth,
+    width: width - 40,
+    paddingHorizontal: 15,
+    backgroundColor: "#FAFAFA",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  pickerText: {
+    fontSize: 24,
+    color: "#bbb"
+  },
+  pickerIcon: {
+    marginLeft: 46
+  },
+  agreeText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    lineHeight: 24.0,
+    color: "rgba(0, 0, 0, 0.38)"
+  },
+  moreText: {
+    fontSize: 14,
+    color: "rgba(0, 0, 0, 0.38)",
+    marginTop: 13,
+    textDecorationLine: "underline"
+  },
+  signinButton: {
+    width: width-40,
+    height: 52,
+    borderRadius: 5,
+    backgroundColor: "#4990e2",
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: "center"
-  }
+    marginTop: 23,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  signinText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textAlign: "center",
+    color: "#ffffff"
+  },
 });
 
 export default SignupScreen;
