9ed1fa5994213166dbe8d16c6b79933f70f230c9
“jcgod413” <jcgod413@gmail.com>
2018-02-01 02:31:10 +0900

    Login Screen view

--- navigation/LoggedOutNavigation.js
+++ navigation/LoggedOutNavigation.js
@@ -3,25 +3,30 @@ import IntroScreen from "../screens/IntroScreen";
 import LoginScreen from "../screens/LoginScreen";
 import SignupScreen from "../screens/SignupScreen";
 
-const LoggedOutNavigation = StackNavigator({
-  Intro: {
-    screen: IntroScreen,
-    navigationOptions: {
-      header: null
+const LoggedOutNavigation = StackNavigator(
+  {
+    Intro: {
+      screen: IntroScreen,
+      navigationOptions: {
+        header: null
+      }
+    },
+    Login: {
+      screen: LoginScreen,
+      mode: "modal",
+      navigationOptions: {
+        header: null
+      }
+    },
+    Signup: {
+      screen: SignupScreen,
+      navigationOptions: {
+        header: null,
+        mode: "modal"
+      }
     }
   },
-  Login: {
-    screen: LoginScreen,
-    navigationOptions: {
-      header: null
-    }
-  },
-  Signup: {
-    screen: SignupScreen,
-    navigationOptions: {
-      header: null
-    }
-  }
-});
+  { mode: "modal" }
+);
 
 export default LoggedOutNavigation;
